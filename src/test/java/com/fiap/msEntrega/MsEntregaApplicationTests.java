65d8761ee0c2263400f7ae9ae55ffb6342515fda
Deverlyn Christine <deverlyn.cgoncalves@gmail.com>
2024-12-01 22:13:33 -0300

    Initial commit

---
+++ src/test/java/com/fiap/msEntrega/MsEntregaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.msEntrega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEntregaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
